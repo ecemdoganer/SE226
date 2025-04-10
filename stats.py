fe1a3822d88678fb7f918dcbf207926b4fa66d63
Ecem Doğaner <120109387+ecemdoganer@users.noreply.github.com>
2025-04-10 03:41:40 -0700

    Create stats.py

---
+++ stats.py
@@ -0,0 +1,14 @@
+def count_characters(s):
+    return len()
+
+def count_words(s):
+    return len(s.split())
+
+def average_words_length(s):
+    words = s.split()
+
+if __name__ == '__main__':
+    s = "Ecem"
+
+    print(count_characters(s))
+    print(count_words(s))
